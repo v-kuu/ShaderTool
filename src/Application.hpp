df85b982ae076f2620360201c2ef027912a5ba0d
v-kuu <vilku@proton.me>
2026-03-18 15:20:13 +0200

    Fixes to CMake and included glm

--- src/Application.hpp
+++ src/Application.hpp
@@ -5,6 +5,7 @@
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
+#include <glm/glm.hpp>
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 #define VULKAN_HPP_HANDLE_ERROR_OUT_OF_DATE_AS_SUCCESS
 #include <vulkan/vulkan_raii.hpp>
